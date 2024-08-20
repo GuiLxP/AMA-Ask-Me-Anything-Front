6381c0f1a0261c132f89c961364e0a26ba4a2f98
GuiLxP <guilhermematheus166@gmail.com>
2024-08-20 18:33:36 -0300

    Implementing dark mode on/off.

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,38 +1,49 @@
 import { ArrowRight } from 'lucide-react'
+import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
 
 import amaLogo from '../assets/ama-logo.svg'
-import { useNavigate } from 'react-router-dom'
+import { createRoom } from '../http/create-room'
 
 export function CreateRoom() {
   const navigate = useNavigate()
 
-  function handleCreateRoom(data: FormData) {
+  async function handleCreateRoom(data: FormData) {
     const theme = data.get('theme')?.toString()
 
-    console.log(theme)
+    if (!theme) {
+      return
+    }
 
-    navigate('/room/12893uqwu8fhsdaf')
+    try {
+      const { roomId } = await createRoom({ theme })
+
+      navigate(`/room/${roomId}`)
+    } catch {
+      toast.error('Falha ao criar sala!')
+    }
   }
 
   return (
-    <main className="h-screen flex items-center justify-center px-4">
+    <main className="bg-white dark:bg-black h-screen flex items-center justify-center px-4">
       <div className="max-w-[450px] flex flex-col gap-6">
         <img src={amaLogo} alt="AMA" className="h-10" />
 
-        <p className="leading-relaxed text-zinc-300 text-center">
+        <p className="leading-relaxed text-zinc-900 dark:text-zinc-300 text-center">
           Crie uma sala pública de AMA (Ask me anything) e priorize as perguntas mais importantes para a comunidade.
         </p>
 
         <form 
           action={handleCreateRoom}
-          className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
+          className="flex items-center gap-2 dark:bg-zinc-900 p-2 rounded-xl border border-zinc-300 dark:border-zinc-800 ring-orange-400 ring-offset-2 dark:ring-offset-zinc-950 focus-within:ring-1"
         >
           <input 
             type="text" 
             name="theme"
             placeholder="Nome da sala"
             autoComplete="off"
-            className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
+            className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-900 dark:text-zinc-100 placeholder:text-zinc-600 dark:placeholder:text-zinc-500"
+            required
           />
 
           <button 
